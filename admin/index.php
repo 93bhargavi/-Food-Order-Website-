367356da59ea26573600a351b6b8a536e26a24fd
93bhargavi <137365939+93bhargavi@users.noreply.github.com>
2023-10-15 21:15:52 +0530

    index file created

---
+++ admin/index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+    <title>Food Order Website - Home Page</title>
+      
+     <link rel="stylessheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!-- Menu Section Starts -->
+        <div class="menu">
+            <div class="wrapper"> 
+                Menu Goes Here
+            </div>
+        </div>
+        <!-- Menu Section Ends -->
+
+        <!-- Main content section starts -->
+        <div class="main">
+            Main Content Goes Here
+        </div>
+        <!-- Main content section ends -->
+
+        <!-- Footer section starts-->
+        <div class="footer">
+            <div class="wrapper"> 
+                <p>2023 All rights reserved and Developed By - <a href="#"> Bhargavi ></a></p>
+             </div>
+        </div>
+        <!-- Footer section ends -->
+     </body>
+ </html>
+ 
