d24b8b8dcfcb3bef5a7b2889620f6bdedc60644a
93bhargavi <137365939+93bhargavi@users.noreply.github.com>
2023-10-31 22:32:45 +0530

    created php file

---
+++ css/Admin/index.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+    <title>Food Order Website - Home Page</title>
+     
+     <link rel="stylessheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!-- Menu Section Starts -->
+        <div class="menu text-center">
+            <div class="wrapper">
+                <ul>
+                    <li>Home</li>
+                    <li>Admin</li>
+                    <li>Category</li>
+                    <li>Food</li>
+                    <li>Order</li>
+                </ul>
+             </div>
+        </div>
+        <!-- Menu Section Ends -->
+
+
+        <!-- Main content section starts -->
+        <div class="main">
+            Main Content Goes Here
+        </div>
+        <!-- Main content section ends -->
+
+
+        <!-- Footer section starts-->
+        <div class="footer">
+            <div class="wrapper">
+                <p>2023 All rights reserved and Developed By - <a href="#"> Bhargavi ></a></p>
+             </div>
+        </div>
+        <!-- Footer section ends -->
+     </body>
+ </html>
+
+ 
+
